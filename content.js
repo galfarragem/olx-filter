e8ef5f3cad3f41de1f681a0a59b40138cea500f3
E. Ferreira <johnnyzeze1+gi@gmail.com>
2018-02-10 18:53:57 +0000

    +004

--- content.js
+++ content.js
@@ -1,7 +1,9 @@
 // jquery selector hiding table rows containing provided words
 
-var stringsToHideArray = ["Carros", "Bebé", "Moda", "Roupa", "Filmes", "Calçado", "Animais", "Desportos de Combate", "Camiões"];
+var stringsToHide = ["Carros", "Bebé", "Moda", "Roupa", "Filmes", "Calçado", "Animais", "Desportos de Combate", 
+"Camiões", "Aves", "Brinquedos", "CDS", "Huawei", "Android", "Guitarra", "Nokia", "Prato", "Copo", "Reparações e Mudanças",
+"relógio", "Relógio", "Fotocopiadoras", "Loiça", "loiça", "Jarro", "cristal", "Consolas", "Saúde - Beleza"];
 
-var preparedString = stringsToHideArray.map(word => "tr:contains(" + word + ")").join();
+var preparedString = stringsToHide.map(word => "tr:contains(" + word + ")").join();
 
 $(preparedString).toggle();
